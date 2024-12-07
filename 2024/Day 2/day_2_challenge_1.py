64ab82a13a7d67951b78ef9d372bf94fb2a8fcb6
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 22:11:50 +0000

    Added parsing of input data

--- 2024/Day 2/day_2_challenge_1.py
+++ 2024/Day 2/day_2_challenge_1.py
@@ -8,3 +8,30 @@ from day_2_input_data import input_data
 
 data = input_data.input
 
+report_list = []
+reports = []
+levels = []
+
+## Creates list of reports
+report_list = data.split("\n")
+
+## Creates nested list of reports
+for report in report_list:
+    temp_report_list = report.split(" ")     
+    reports.append(temp_report_list)
+
+
+for report in reports:
+    print(report)
+    level_list = []
+    for str_level in report:
+        print(str_level)       
+        level_list.append(int(str_level))
+        print(level_list)
+    levels.append(level_list)
+        
+
+    print("----------------------")
+
+#######################################################
+
