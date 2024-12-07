51d6289527ef5cdb0b6cecc19ad63c908cdb6a67
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 23:53:26 +0000

    Cleaned up version that just outputs the final count

--- 2024/Day 2/day_2_challenge_1.py
+++ 2024/Day 2/day_2_challenge_1.py
@@ -1,19 +1,7 @@
 from day_2_input_data import input_data
-import logging
+
 
 ## Challenge URL: https://adventofcode.com/2024/day/2
-
-#######################################################
-
-logging.basicConfig(
-    filename="Day_2_Challenge_2.log",
-    encoding="utf-8",
-    filemode="a",
-    format="%(asctime)s.%(msecs)05d - %(levelname)s - %(message)s",
-    style="%",
-    datefmt="%Y-%m-%dT%H:%M:%S",
-    level=logging.DEBUG,
-)
 
 #######################################################
 
@@ -35,35 +23,24 @@ for report in report_list:
 
 ## Creates nested list of levels
 for report in reports:
-    logging.debug(report)
     level_list = []
-    for str_level in report:
-        logging.debug(str_level)       
+    for str_level in report:    
         level_list.append(int(str_level))
-        logging.debug(level_list)
     levels.append(level_list)
-        
-
-    logging.debug("----------------------")
 
 #######################################################
 
 ## Performing the actual logic for the puzzle
 list_appended = 0
-debug_broken = 0
 valid_levels = []
 
 for report in levels:
-    logging.debug("---------------------------------------")
-    logging.debug(report)
     previous_level = None
     checked = 0
     rule_broken = False
     level_was_the_same = None
     valid_level_list = []
     for level in report:
-        logging.debug("---------------------------------------")
-        logging.debug(f"Previous Level: {previous_level}")
         # Ensures that the level is skipped if the rule was broken 
         if rule_broken:
             break
@@ -71,21 +48,16 @@ for report in levels:
         if previous_level == None and checked == 0:
             checked += 1
             previous_level = level
-            logging.debug(f"SKIPPED {level}")
             continue
-        else:
-            logging.debug(f"LEVEL: {level}")
 
         # It can stop checking here for this level if the level and previous level are the same
         if previous_level == level:
-            logging.debug(f"Previous Level: {previous_level} and Level: {level} are the same")
             rule_broken = True
             break
 
         # This figures out whether the report should be checking for ascending or descending numbers
         if checked == 1:
             if previous_level == level:
-                logging.debug(f"Previous Level: {previous_level} and Level: {level} are the same")
                 continue
             else:
                 checked += 1
@@ -93,17 +65,13 @@ for report in levels:
                     up_or_down = True 
                 elif previous_level > level:
                     up_or_down = False
-                logging.debug(f"Up or down? {up_or_down}")
 
-        
-
+        # This performs the log to make sure that the numbers are within the acceptable parameters
         if up_or_down:
             if level < previous_level:
                 rule_broken = True
                 break
             elif level - previous_level > 3:
-                logging.debug("Rule Broken")
-                debug_broken += 1
                 rule_broken = True
                 break
             else:
@@ -113,28 +81,21 @@ for report in levels:
                 rule_broken = True
                 break
             elif previous_level - level > 3:
-                logging.debug("Rule Broken")
-                debug_broken += 1
                 rule_broken = True
                 break
             else:
                 valid_level_list.append(level)
 
-        previous_level = level  
-        logging.debug(f"Valid List: {valid_level_list}")      
+        previous_level = level       
 
+    # Adds the list to the list for valid reports
     if valid_level_list != [] and rule_broken == False:
         valid_levels.append(valid_level_list)
-        logging.debug("LIST APPENDED")
         list_appended += 1
-logging.debug("---------------------------------------")
-logging.debug(valid_levels)
-logging.debug(list_appended)
-logging.debug(debug_broken)
 #######################################################
 
 ## Final Count 
 
 valid_level_count = len(valid_levels)
 
-logging.debug(valid_level_count)
+print(valid_level_count)
