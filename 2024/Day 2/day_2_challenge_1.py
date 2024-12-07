9015a730d7073aa290a56aa1ebebed89b8b19456
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 23:49:38 +0000

    Finally got it! I forgot to actually check if the numbers were ascending or not!

--- 2024/Day 2/day_2_challenge_1.py
+++ 2024/Day 2/day_2_challenge_1.py
@@ -9,9 +9,9 @@ logging.basicConfig(
     filename="Day_2_Challenge_2.log",
     encoding="utf-8",
     filemode="a",
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    format="%(asctime)s.%(msecs)05d - %(levelname)s - %(message)s",
     style="%",
-    datefmt="%Y-%m-%dT%H:%M",
+    datefmt="%Y-%m-%dT%H:%M:%S",
     level=logging.DEBUG,
 )
 
@@ -35,16 +35,16 @@ for report in report_list:
 
 ## Creates nested list of levels
 for report in reports:
-    print(report)
+    logging.debug(report)
     level_list = []
     for str_level in report:
-        print(str_level)       
+        logging.debug(str_level)       
         level_list.append(int(str_level))
-        print(level_list)
+        logging.debug(level_list)
     levels.append(level_list)
         
 
-    print("----------------------")
+    logging.debug("----------------------")
 
 #######################################################
 
@@ -54,17 +54,16 @@ debug_broken = 0
 valid_levels = []
 
 for report in levels:
-    print("---------------------------------------")
-    print(report)
+    logging.debug("---------------------------------------")
+    logging.debug(report)
     previous_level = None
     checked = 0
     rule_broken = False
     level_was_the_same = None
     valid_level_list = []
     for level in report:
-        print("---------------------------------------")
         logging.debug("---------------------------------------")
-        print(f"Previous Level: {previous_level}")
+        logging.debug(f"Previous Level: {previous_level}")
         # Ensures that the level is skipped if the rule was broken 
         if rule_broken:
             break
@@ -72,15 +71,21 @@ for report in levels:
         if previous_level == None and checked == 0:
             checked += 1
             previous_level = level
-            print(f"SKIPPED {level}")
+            logging.debug(f"SKIPPED {level}")
             continue
         else:
-            print(f"LEVEL: {level}")
+            logging.debug(f"LEVEL: {level}")
+
+        # It can stop checking here for this level if the level and previous level are the same
+        if previous_level == level:
+            logging.debug(f"Previous Level: {previous_level} and Level: {level} are the same")
+            rule_broken = True
+            break
 
         # This figures out whether the report should be checking for ascending or descending numbers
         if checked == 1:
             if previous_level == level:
-                print(f"Previous Level: {previous_level} and Level: {level} are the same")
+                logging.debug(f"Previous Level: {previous_level} and Level: {level} are the same")
                 continue
             else:
                 checked += 1
@@ -88,27 +93,27 @@ for report in levels:
                     up_or_down = True 
                 elif previous_level > level:
                     up_or_down = False
-                print(f"Up or down? {up_or_down}")
+                logging.debug(f"Up or down? {up_or_down}")
 
-        # It can stop checking here for this level if the level and previous level are the same
-        if previous_level == level:
-            print(f"Previous Level: {previous_level} and Level: {level} are the same")
-            level_was_the_same = True
-            continue
+        
 
         if up_or_down:
-            if level - previous_level > 3:
+            if level < previous_level:
+                rule_broken = True
+                break
+            elif level - previous_level > 3:
                 logging.debug("Rule Broken")
-                print("Rule Broken")
                 debug_broken += 1
                 rule_broken = True
                 break
             else:
                 valid_level_list.append(level)
         else:
-            if previous_level - level > 3:
+            if level > previous_level:
+                rule_broken = True
+                break
+            elif previous_level - level > 3:
                 logging.debug("Rule Broken")
-                print("Rule Broken")
                 debug_broken += 1
                 rule_broken = True
                 break
@@ -116,21 +121,20 @@ for report in levels:
                 valid_level_list.append(level)
 
         previous_level = level  
-#        print(f"Valid List: {valid_level_list}")      
-#
-#    if valid_level_list != []:
-#        valid_levels.append(valid_level_list)
-#        print("LIST APPENDED")
-#        list_appended += 1
-#print("---------------------------------------")
-#logging.debug("---------------------------------------")
-#print(valid_levels)
-#print(list_appended)
-#print(debug_broken)
+        logging.debug(f"Valid List: {valid_level_list}")      
+
+    if valid_level_list != [] and rule_broken == False:
+        valid_levels.append(valid_level_list)
+        logging.debug("LIST APPENDED")
+        list_appended += 1
+logging.debug("---------------------------------------")
+logging.debug(valid_levels)
+logging.debug(list_appended)
+logging.debug(debug_broken)
 #######################################################
 
 ## Final Count 
 
-valid_level_count = len(levels) - debug_broken
+valid_level_count = len(valid_levels)
 
-print(valid_level_count)
+logging.debug(valid_level_count)
