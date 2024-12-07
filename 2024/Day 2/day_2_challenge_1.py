74aff6afff64f15c076ea6c50ef307e279ae1473
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 21:30:40 +0000

    Starting day 2, challenge 1

---
+++ 2024/Day 2/day_2_challenge_1.py
@@ -0,0 +1,10 @@
+from day_2_input_data import input_data
+
+## Challenge URL: https://adventofcode.com/2024/day/2
+
+#######################################################
+
+## Turning the input data reports and levels
+
+data = input_data.input
+
