c73fe87920d7e375c03695026b7b79a6063a23b1
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 23:07:17 +0000

    Added shoddy but working logic for the main checks

--- 2024/Day 2/day_2_challenge_1.py
+++ 2024/Day 2/day_2_challenge_1.py
@@ -20,7 +20,7 @@ for report in report_list:
     temp_report_list = report.split(" ")     
     reports.append(temp_report_list)
 
-
+## Creates nested list of levels
 for report in reports:
     print(report)
     level_list = []
@@ -35,3 +35,64 @@ for report in reports:
 
 #######################################################
 
+valid_levels = []
+
+for report in levels:
+    print(report)
+    previous_level = None
+    checked = 0
+    rule_broken = False
+    level_was_the_same = None
+    valid_level_list = []
+    for level in report:
+        # Ensures that the level is skipped if the rule was broken 
+        if rule_broken:
+            break
+        # Skips the first number in each report as we dont need to check it
+        if previous_level == None and checked == 0:
+            checked += 1
+            previous_level = level
+            print(f"SKIPPED {level}")
+            continue
+        else:
+            print(f"LEVEL: {level}")
+
+        # This figures out whether the report should be checking for ascending or descending numbers
+        if checked == 1:
+            if previous_level == level:
+                continue
+            else:
+                checked += 1
+                if previous_level < level:
+                    up_or_down = True 
+                elif previous_level > level:
+                    up_or_down = False
+
+        # It can stop checking here for this level if the level and previous level are the same
+        if previous_level == level:
+            print(f"Previous Level: {previous_level} and Level: {level} are the same")
+            level_was_the_same = True
+            continue
+
+        if up_or_down:
+            if level - previous_level > 3:
+                rule_broken = True
+                break
+            else:
+                valid_level_list.append(level)
+        else:
+            if previous_level - level > 3:
+                rule_broken = True
+                break
+            else:
+                valid_level_list.append(level)
+
+        previous_level = level        
+
+    if valid_level_list != []:
+        valid_levels.append(valid_level_list)
+
+
+#######################################################
+
+## Final Count 
