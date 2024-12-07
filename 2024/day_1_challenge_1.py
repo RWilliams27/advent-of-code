d7a73391282fc908a781274115a3b59bedc8e6f0
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 21:04:28 +0000

    Distance measuring

--- 2024/day_1_challenge_1.py
+++ 2024/day_1_challenge_1.py
@@ -16,7 +16,33 @@ for item in input_list:
 
     if (i % 2) != 0:
         list_a.append(item)
+        print(f"Number: {item} was added to List A")
     else:
         list_b.append(item)
+        print(f"Number: {item} was added to List B")
+
+#######################################################
+
+## Sorting lists by ascending value
+list_a.sort()
+list_b.sort()
+
+## Calculating the distance between each pair and adding it to a list
+distance_list = []
+
+i = 0
+while i < len(list_a):
+    list_a_item = int(list_a[i])
+    list_b_item = int(list_b[i])
+    print(list_a_item, list_b_item)
+
+    if list_a_item < list_b_item:
+        distance_list.append(list_b_item - list_a_item)
+    else:
+        distance_list.append(list_a_item - list_b_item)
+    print(f"Distance of pair {i} is {distance_list[i]}")
+
+
+    i += 1
 
 #######################################################
