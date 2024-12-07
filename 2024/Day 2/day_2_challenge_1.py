538ffefc2af56070a2397976b0a1d97407e6af59
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-07 23:31:42 +0000

    Debugging before I realised I misunderstood one of the rules

--- 2024/Day 2/day_2_challenge_1.py
+++ 2024/Day 2/day_2_challenge_1.py
@@ -1,6 +1,19 @@
 from day_2_input_data import input_data
+import logging
 
 ## Challenge URL: https://adventofcode.com/2024/day/2
+
+#######################################################
+
+logging.basicConfig(
+    filename="Day_2_Challenge_2.log",
+    encoding="utf-8",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    style="%",
+    datefmt="%Y-%m-%dT%H:%M",
+    level=logging.DEBUG,
+)
 
 #######################################################
 
@@ -35,9 +48,13 @@ for report in reports:
 
 #######################################################
 
+## Performing the actual logic for the puzzle
+list_appended = 0
+debug_broken = 0
 valid_levels = []
 
 for report in levels:
+    print("---------------------------------------")
     print(report)
     previous_level = None
     checked = 0
@@ -45,6 +62,9 @@ for report in levels:
     level_was_the_same = None
     valid_level_list = []
     for level in report:
+        print("---------------------------------------")
+        logging.debug("---------------------------------------")
+        print(f"Previous Level: {previous_level}")
         # Ensures that the level is skipped if the rule was broken 
         if rule_broken:
             break
@@ -60,6 +80,7 @@ for report in levels:
         # This figures out whether the report should be checking for ascending or descending numbers
         if checked == 1:
             if previous_level == level:
+                print(f"Previous Level: {previous_level} and Level: {level} are the same")
                 continue
             else:
                 checked += 1
@@ -67,6 +88,7 @@ for report in levels:
                     up_or_down = True 
                 elif previous_level > level:
                     up_or_down = False
+                print(f"Up or down? {up_or_down}")
 
         # It can stop checking here for this level if the level and previous level are the same
         if previous_level == level:
@@ -76,27 +98,39 @@ for report in levels:
 
         if up_or_down:
             if level - previous_level > 3:
+                logging.debug("Rule Broken")
+                print("Rule Broken")
+                debug_broken += 1
                 rule_broken = True
                 break
             else:
                 valid_level_list.append(level)
         else:
             if previous_level - level > 3:
+                logging.debug("Rule Broken")
+                print("Rule Broken")
+                debug_broken += 1
                 rule_broken = True
                 break
             else:
                 valid_level_list.append(level)
 
-        previous_level = level        
-
-    if valid_level_list != []:
-        valid_levels.append(valid_level_list)
-
-
+        previous_level = level  
+#        print(f"Valid List: {valid_level_list}")      
+#
+#    if valid_level_list != []:
+#        valid_levels.append(valid_level_list)
+#        print("LIST APPENDED")
+#        list_appended += 1
+#print("---------------------------------------")
+#logging.debug("---------------------------------------")
+#print(valid_levels)
+#print(list_appended)
+#print(debug_broken)
 #######################################################
 
 ## Final Count 
 
-valid_level_count = len(valid_levels)
+valid_level_count = len(levels) - debug_broken
 
 print(valid_level_count)
