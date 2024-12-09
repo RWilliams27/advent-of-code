67abd79159c6b30d8e78a22bd219e9d8134166a4
RWilliams27 <rwilliams@jwplayer.com>
2024-12-09 11:25:55 +0000

    This was a quicker one!

--- 2024/Day 3/day_3_challenge_1.py
+++ 2024/Day 3/day_3_challenge_1.py
@@ -8,6 +8,29 @@ import re
 data = input_data.input
 
 # Needed regex for finding the valid mul statements
-x = re.findall(r"mul\(\d+,\s*\d+\)", data)
+valid_mul = re.findall(r"mul\(\d+,\s*\d+\)", data)
 
-print(x)
+add_list = []
+
+for mul in valid_mul:
+    # Turns the fetched "valid-muls" into pairs of numbers
+    output = mul.lstrip("mul(")
+    output = output.rstrip(")")
+    output = output.split(",")
+
+    # Turn all the numbers into integers
+    output[0] = int(output[0])
+    output[1] = int(output[1])
+
+    # Multiplies all the pairs
+    output = output[0] * output [1]
+
+    add_list.append(output)
+
+# Add all the multiplied figures together
+final_count = 0
+
+for count in add_list:
+    final_count += count
+
+print(final_count)
