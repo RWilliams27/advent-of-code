34444b69da9c84d67464ce5e400fc89825304b17
RWilliams27 <rwilliams@jwplayer.com>
2024-12-09 12:12:47 +0000

    Ive misunderstood the task. Its only the most recent do/dont not all that I am searching for

--- 2024/Day 3/day_3_challenge_2.py
+++ 2024/Day 3/day_3_challenge_2.py
@@ -7,7 +7,7 @@ import re
 
 data = input_data.input
 
-#data = "carrot don't() dasfa do() juniper don't() eggs final"
+data = """}mul(620,236)where()*@}!&[mul(589,126)]&^]mul(260,42)when() when()$ ?{/^*mul(335,250)>,@!<{when()+-$don't()*'^?+>>/%:mul(422,738),mul(694,717);~;%<[why()>@-mul(417,219)?&who(474,989){select()-{#mul(366,638)mul(773,126)/*{mul(757,799)]when()mul(778,467)^mul(487,365)]*'{where(952,954){?who()who()when()mul(172,666)#<do()why()~&why())'< {mul(33,475)}mul(916,60)what()?when()>?$,-mul(250,228)(]when()}<mul(817,274)'{})mul(836,930):@how()]!@'select()~?mul(514,457)from()&what()what()when()mul(872,884)select()<select()from()'!who()mul(11,966)/from()(~}#,(*from()mul(941,908)#>mul(760,139)mul(892,161)!'@[%when()<(mul(775,872)+~#)//$select()mul(946,63)how()??select()?from(277,915)~'mul(637,565)~mul(881,294)who()what()}mul(995,866)?mul(952,57)who()mul(387,599)mul(46,724)who()[how()select()mul(992,19)'~mul(909,687)where()mul(953,804)from()/;where(474,270)from()}mul(907,410)&(&what()%{mul(192,898)who()-,mul(196,400)#--{%]how()mul(144,141)~@[when()!%:[mul(377,942)^*mul(89,46)who()<}when()?!'%mul(172,448){]@mul(351,18)~]&!$mul(490,127)/]] }}mul(851,465)when()*-why()what()))@+<mul(465,978):*>^<-select()do()%#+;%:mul(549,307)<where(154,242)(;< /who()mul(426,943)mul(477,782)*?do() mul(745,445)@  (how()$where()mul(118,902)when()when()}!how()don't();mul(523,781)"""
 #
 ### Find a way to parse only the valid bits before the rest of the logic
 #
@@ -45,6 +45,7 @@ def mult(mul):
     output = mul.lstrip("mul(")
     output = output.rstrip(")")
     output = output.split(",")
+    print(output)
 
     # Turn all the numbers into integers
     output[0] = int(output[0])
@@ -62,6 +63,8 @@ print(first_valid_mul)
 
 stringified_further_data = str(further_data)
 
+print(stringified_further_data)
+
 valid_mul = re.findall(r"mul\(\d+,\s*\d+\)", stringified_further_data)
 
 for mul in first_valid_mul:
@@ -75,5 +78,7 @@ final_count = 0
 
 for count in add_list:
     final_count += count
+
+
 
 print(final_count)
