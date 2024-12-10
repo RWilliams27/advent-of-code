8e189f1d9951af613c6e3b77d20cbbb5e706e33a
RWilliams27 <rwilliams@jwplayer.com>
2024-12-10 13:05:41 +0000

    And it got the input right!!! Plus some tidying

--- 2024/Day 4/day_4_challenge_2.py
+++ 2024/Day 4/day_4_challenge_2.py
@@ -6,34 +6,21 @@ from day_4_input_data import input_data
 
 data = input_data.input
 
-data = """MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX"""
-
-## Ideas
-# Maybe it into a nested array with each line being an array to essentially create a grid?
-# What if I check for each kind of xmas individually and then add up the final count? (IE: horizontal, then downwards, then upwards, then backwards, then diagonal)
-## How do I achieve that?
-# Backwards and forwards are easy, just do count() for them
-# Downwards and upwards, I think the best method will be to do a detection for either "x" or "s" and then check the same index in each array and confirm if the word exists by checking for each letter
-# Diagonal, I will need to check the previous and next index in each array and confirm if the word exists by checking for each letter
+#data = """MMMSXXMASM
+#MSAMXMSMSA
+#AMXSXMAAMM
+#MSAMASMSMX
+#XMASAMXAMM
+#XXAMMXXAMA
+#SMSMSASXSS
+#SAXAMASAAA
+#MAMMMXMMMM
+#MXMXAXMASX"""
 
 ## Turning the data into a 2d array (grid) to visualise
 no_new_lines = [row.strip() for row in data.split("\n")]
 
-print(no_new_lines)
-
 grid = [[0 for i in range(len(no_new_lines[0]))] for j in range(len(no_new_lines))]
-
-for row in grid:
-    print(row)
 
 row_iterate = 0
 while row_iterate < len(grid):
@@ -44,71 +31,6 @@ while row_iterate < len(grid):
 
         column_iterate += 1
     row_iterate += 1
- 
-for i in grid:
-    print(i)
-
-
-horizontal_count = 0
-backwards_count = 0
-
-for row in grid:
-    row_str = ""
-
-    for i in row:
-        row_str = row_str + i
-
-    horizontal_count += row_str.count("XMAS")
-    backwards_count += row_str.count("SAMX")
-
-    print(f"Row String: {row_str}")
-
-
-schema_dict = {
-    "vertical": {
-        "upwards":[0, 0, 0, 0],
-        "downwards": [0, 0, 0, 0]
-        },
-    "diagonal": {
-        "left": [0, -1, -2, -3],
-        "right": [0, 1, 2, 3]
-    },
-    "letters": {
-        "normal": ["X", "M", "A", "S"],
-        "reverse": ["S", "A", "M", "X"]
-    }
-}
-
-def wordsearch(direction, letter_order): # Will also need to add the dict I want appended to 
-    wordCount = 0
-    row_index = 0
-    for row in grid:
-        column_index = 0
-        for i in row:
-            if i == letter_order[0]:
-
-                if row_index + 1 < len(grid) and column_index + direction[1] < len(row) and column_index + direction[1] >= 0:
-                    if grid[row_index + 1][column_index + direction[1]] == letter_order[1]:
-
-                        if row_index + 2 < len(grid) and column_index + direction[2] < len(row) and column_index + direction[2] >= 0:
-                            if grid[row_index + 2][column_index + direction[2]] == letter_order[2]:
-
-                                if row_index + 3 < len(grid) and column_index + direction[3] < len(row) and column_index + direction[3] >= 0:
-                                    if grid[row_index + 3][column_index + direction[3]] == letter_order[3]:
-                                        #print("----------------------")
-                                        #print(f"ROW INDEX: {row_index}   |   {row}")
-                                        #print(f"ROW INDEX: {row_index + 1}   |   {grid[row_index + 1]}")
-                                        #print(f"ROW INDEX: {row_index + 2}   |   {grid[row_index + 2]}")
-                                        #print(f"ROW INDEX: {row_index + 3}   |   {grid[row_index + 3]}")
-                                        #print("----------------------")  
-                                        wordCount += 1
-
-            column_index += 1
-
-        row_index += 1
-    
-    return wordCount
-
 
 ## Ideas
 # Look for "A"'s and then check the diagonal figures for the "S"'s and "M"'s
@@ -120,84 +42,7 @@ def wordsearch(direction, letter_order): # Will also need to add the dict I want
 # Top-Right
 # Bottom-Right
 
-debug_grid = [[":" for i in range(len(no_new_lines[0]))] for j in range(len(no_new_lines))]
-
-def og_mas_wordsearch():
-    word_count = 0
-    row_index = 0
-
-    for row in grid:
-        column_index = 0
-        for i in row:
-            if i == "A":
-
-                # Top left
-                if row_index - 1 < len(grid) and row_index - 1 >= 0 and column_index - 1 < len(row) and column_index - 1 >= 0:
-                    if grid[row_index - 1][column_index - 1] == "S":
-
-                        # Bottom Right
-                        if row_index + 1 < len(grid) and row_index + 1 >= 0 and column_index + 1 < len(row) and column_index + 1 >= 0:
-                            if grid[row_index + 1][column_index + 1] == "M":
-                                print("FOUND")
-                                word_count += 1
-
-                                debug_grid[row_index][column_index] = "A"
-                                debug_grid[row_index - 1][column_index - 1] = "S"
-                                debug_grid[row_index + 1][column_index + 1] = "M"
-
-
-
-                # Top Right
-                if row_index - 1 < len(grid) and row_index - 1 >= 0 and column_index + 1 < len(row) and column_index + 1 >= 0:
-                    if grid[row_index - 1][column_index + 1] == "S":
-
-                        # Bottom Left
-                        if row_index + 1 < len(grid) and row_index + 1 >= 0 and column_index - 1 < len(row) and column_index - 1 >= 0:
-                            if grid[row_index + 1][column_index - 1] == "M":
-                                print("FOUND 2")
-                                word_count += 1
-
-                                debug_grid[row_index][column_index] = "A"
-                                debug_grid[row_index - 1][column_index + 1] = "S"
-                                debug_grid[row_index + 1][column_index - 1] = "M"
-
-
-                # Bottom Left
-                if row_index + 1 < len(grid) and row_index + 1 >= 0 and column_index - 1 < len(row) and column_index - 1 >= 0:
-                    if grid[row_index + 1][column_index - 1] == "S":
-
-                        # Top Right
-                        if row_index - 1 < len(grid) and row_index - 1 >= 0 and column_index + 1 < len(row) and column_index + 1 >= 0:
-                            if grid[row_index - 1][column_index + 1] == "M":   
-                                print("FOUND 3")   
-                                word_count += 1        
-
-                                debug_grid[row_index][column_index] = "A"
-                                debug_grid[row_index + 1][column_index - 1] = "S"
-                                debug_grid[row_index - 1][column_index + 1] = "M"      
-
-
-                # Bottom Right
-                if row_index + 1 < len(grid) and row_index + 1 >= 0 and column_index + 1 < len(row) and column_index + 1 >= 0:
-                    if grid[row_index + 1][column_index + 1] == "S":
-
-                        # Top Left
-                        if row_index - 1 < len(grid) and row_index - 1 >= 0 and column_index - 1 < len(row) and column_index - 1 >= 0:
-                            if grid[row_index - 1][column_index - 1] == "S":
-                                print("FOUND 4")
-                                word_count += 1
-
-                                debug_grid[row_index][column_index] = "A"
-                                debug_grid[row_index + 1][column_index + 1] = "S"
-                                debug_grid[row_index - 1][column_index - 1] = "M"
-
-            column_index += 1
-
-        row_index += 1
-
-    return word_count
-
-def gridCheck(direction, letter, row_index, column_index):
+def gridCheck(direction, letter, row_index, column_index, row):
 
     if direction == "Top-Left":
         if row_index - 1 < len(grid) and row_index - 1 >= 0 and column_index - 1 < len(row) and column_index - 1 >= 0:
@@ -232,43 +77,35 @@ def mas_wordsearch():
             if i == "A":
 
                 # Top left S
-                if gridCheck("Top-Left", "S", row_index, column_index):
+                if gridCheck("Top-Left", "S", row_index, column_index, row):
 
-                    if gridCheck("Bottom-Right", "M", row_index, column_index):
+                    if gridCheck("Bottom-Right", "M", row_index, column_index, row):
 
-                        if gridCheck("Top-Right", "S", row_index, column_index):
+                        if gridCheck("Top-Right", "S", row_index, column_index, row):
 
-                            if gridCheck("Bottom-Left", "M", row_index, column_index):
-                                print("FOUND")
+                            if gridCheck("Bottom-Left", "M", row_index, column_index, row):
                                 word_count += 1
 
-                        elif gridCheck("Top-Right", "M", row_index, column_index):
+                        elif gridCheck("Top-Right", "M", row_index, column_index, row):
 
-                            if gridCheck("Bottom-Left", "S", row_index, column_index):
-                                print("FOUND 2")
+                            if gridCheck("Bottom-Left", "S", row_index, column_index, row):
                                 word_count += 1
 
                 # Top left M
 
-                elif gridCheck("Top-Left", "M", row_index, column_index):
+                elif gridCheck("Top-Left", "M", row_index, column_index, row):
 
-                    if gridCheck("Bottom-Right", "S", row_index, column_index):
+                    if gridCheck("Bottom-Right", "S", row_index, column_index, row):
 
-                        if gridCheck("Top-Right", "M", row_index, column_index):
+                        if gridCheck("Top-Right", "M", row_index, column_index, row):
 
-                            if gridCheck("Bottom-Left", "S", row_index, column_index):
-                                print("FOUND 3")
+                            if gridCheck("Bottom-Left", "S", row_index, column_index, row):
                                 word_count += 1
 
-                        elif gridCheck("Top-Right", "S", row_index, column_index):
+                        elif gridCheck("Top-Right", "S", row_index, column_index, row):
 
-                            if gridCheck("Bottom-Left", "M", row_index, column_index):
-                                print("FOUND 4")
+                            if gridCheck("Bottom-Left", "M", row_index, column_index, row):
                                 word_count += 1
-
-
-                
-
 
             column_index += 1
 
@@ -276,41 +113,6 @@ def mas_wordsearch():
 
     return word_count
 
-
-## Misunderstood the statement, they must be an X, they cannot be just diagonal
-
-
-
-
-downwards_count = wordsearch(schema_dict["vertical"]["downwards"], schema_dict["letters"]["normal"])
-upwards_count = wordsearch(schema_dict["vertical"]["upwards"], schema_dict["letters"]["reverse"])
-
-diagonal_left = 0
-diagonal_right = 0
-diagonal_left += wordsearch(schema_dict["diagonal"]["left"], schema_dict["letters"]["normal"])
-diagonal_left += wordsearch(schema_dict["diagonal"]["left"], schema_dict["letters"]["reverse"])
-
-diagonal_right += wordsearch(schema_dict["diagonal"]["right"], schema_dict["letters"]["normal"])
-diagonal_right += wordsearch(schema_dict["diagonal"]["right"], schema_dict["letters"]["reverse"])
-
-
-print(f"horizontal_count: {horizontal_count}")
-print(f"backwards_count: {backwards_count}") 
-print(f"upwards_count: {upwards_count}")    
-print(f"downwards_count: {downwards_count}") 
-print(f"diagonal_left: {diagonal_left}") 
-print(f"diagonal_right: {diagonal_right}") 
-
-print(f"Final Count: {horizontal_count + backwards_count + upwards_count + downwards_count + diagonal_left + diagonal_right}")
-
 words = mas_wordsearch()
 
 print(f"Word Count: {words}")
-
-
-#print("------------------- DEBUG GRID -------------------")
-#
-#for row in debug_grid:
-#    print(row)
-#
-#print("------------------- DEBUG GRID -------------------")
