7f91cb5cb075d4e43e8fcffdd59e5e4bb222500d
RWilliams27 <rwilliams@jwplayer.com>
2024-12-10 14:13:41 +0000

    Forgot to parse input into integers

--- 2024/Day 5/day_5_challenge_1.py
+++ 2024/Day 5/day_5_challenge_1.py
@@ -35,6 +35,7 @@ data = """47|53
 61,13,29
 97,13,75,29,47"""
 
+######################################################
 
 ## Parsing the input
 
@@ -43,6 +44,43 @@ split_data = data.split("\n\n")
 unsanitised_pages = split_data[0]
 unsanitised_updates = split_data[1]
 
-pages = unsanitised_pages.split("\n")
-updates = unsanitised_updates.split("\n")
+string_pages = unsanitised_pages.split("\n")
+string_updates = unsanitised_updates.split("\n")
+
+# Turns the list of strings into actual lists
+updates = []
+for update in string_updates:
+    temp_list = []
+    temp_list_str = update.split(",")
+    
+    for i in temp_list_str:
+        temp_list.append(int(i))
+    updates.append(temp_list)
+
+pages = []
+for page in string_pages:
+    temp_list = []
+    temp_list_str = page.split("|")
+
+    for i in temp_list_str:
+        temp_list.append(int(i))
+
+    pages.append(temp_list)
+    
+######################################################
+
+## Ideas
+# Build a function to create one big list of all pages in their required order
+# Build a function that will take this big list and then check each update and ensure they are in the appropriate order
+
+# Each number should only be entered once so you can always find the index right?
+# function for big list
+def list_sorter(input):
+    final_list = []
+
+    for i in input:
+        pass
+
+#for page in pages:
+#    print(page)
 
