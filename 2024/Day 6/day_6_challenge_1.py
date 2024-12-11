6f4ee6f88afe0fbf35f084bd5795034fd41284d3
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-11 20:57:20 +0000

    Comitting before going bed

--- 2024/Day 6/day_6_challenge_1.py
+++ 2024/Day 6/day_6_challenge_1.py
@@ -74,37 +74,50 @@ def move(move_direction, direction):
         return move_direction + 1
 
 def main(curr_dir: int, start_pos: tuple = None, grid = grid):
-    logging.debug(f"Current Dir: {directions[curr_dir]}")
     previous_grid = grid
     player_row, player_column = directions[curr_dir]
+    logging.debug(f"Current Dir: {directions[curr_dir]}")
+            
+    logging.debug("----------------------------")
+    for row in grid:
+        logging.debug(row)
+    logging.debug("----------------------------")  
     for row_index, row in enumerate(grid):
+        logging.debug(f"ROW INDEX: {row_index}")
         for column_index, item in enumerate(row):
-            if previous_grid != grid:
-                logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
-                logging.debug("----------------------------")
-                for row in grid:
-                    logging.debug(row)
-                logging.debug("----------------------------")
+            logging.debug(f"ROW INDEX: {row_index}  |   COLUMN INDEX: {column_index}")
+            #if previous_grid != grid:
+            #    logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
+            #    logging.debug("----------------------------")
+            #    for row in grid:
+            #        logging.debug(row)
+            #    logging.debug("----------------------------")
             if item == 2:
                 if row_index + player_row < len(previous_grid) and row_index + player_row >= 0 and column_index + player_column < len(row) and column_index + player_column >= 0: #Doesnt care if there is a 1 in the way but does go all the way up now
+                    logging.debug("HIT 1")
                     #logging.debug(f"Next position in previous_grid: {previous_grid[row_index + player_row][column_index + player_column]}")
                     if previous_grid[row_index + player_row][column_index + player_column] == 1:
-                        curr_dir = move(curr_dir, 1)                        
+                        logging.debug(f"Player Row: {player_row}    |   Player Column: {player_column}")
+                        curr_dir = move(curr_dir, 1)           
+                        logging.debug("HIT 2")             
                     else: 
+                        logging.debug(f"Player Row: {player_row}    |   Player Column: {player_column}")
                         previous_grid[row_index][column_index] = 0
                         previous_grid[row_index + player_row][column_index + player_column] = 2
+                        logging.debug("HIT 3")
 
                 else:
+                    logging.debug("HIT 4")
                     return curr_dir, grid 
 
     grid = previous_grid 
 
-    logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
-    logging.debug("----------------------------")
-    for row in grid:
-        logging.debug(row)
-    logging.debug("----------------------------")
-
+    #logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
+    #logging.debug("----------------------------")
+    #for row in grid:
+    #    logging.debug(row)
+    #logging.debug("----------------------------")
+    logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")  
     #time.sleep(2)
     return curr_dir, grid
 
