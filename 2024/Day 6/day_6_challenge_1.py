eee8f10a9320a6c43f1120eac04ad1de34bc56fa
Reece <110046204+RWilliams27@users.noreply.github.com>
2024-12-11 19:17:38 +0000

    Added more of the navigating logic

--- 2024/Day 6/day_6_challenge_1.py
+++ 2024/Day 6/day_6_challenge_1.py
@@ -1,5 +1,6 @@
 from day_6_input_data import input_data
 import logging, time
+from typing import Union
 
 ## Challenge URL: https://adventofcode.com/2024/day/6
 
@@ -29,19 +30,20 @@ data = """....#.....
 #.........
 ......#..."""
 
-print("")
+logging.debug("")
 rows = data.split("\n")
 
 # Declares 2d array
 grid = [[0 for i in range(len(rows[0]))] for j in range(len(rows))]
 
-directions = {
-    "up": (-1, 0),
-    "down": (+1, 0),
-    "right": (0, +1),
-    "left": (0, -1)
-}
-curr_dir = None
+#"up": (-1, 0)
+#"down": (+1, 0)
+#"right": (0, +1)
+#"left": (0, -1)
+
+directions = [(-1, 0), (0, +1), (+1, 0),  (0, -1)]
+
+current_direction = 0
 # Translates input map into a 2d array
 for row_index, row in enumerate(rows):
     obstacles = []
@@ -49,56 +51,75 @@ for row_index, row in enumerate(rows):
     for index, item in enumerate(row):
         if item == "#":
             grid[row_index][index] = 1
-        if item == "^": #This wont check direction yet
+        if item == "^": #Hardcoded to start as up
             grid[row_index][index] = 2
-            curr_dir = directions["up"]
-            print(curr_dir)
+            logging.debug(directions[current_direction])
 
-print("----------------------------")
+logging.debug("----------------------------")
 for row in grid:
-    print(row)
-print("----------------------------")
+    logging.debug(row)
+logging.debug("----------------------------")
 
-def move(start_pos: tuple = None):
-    print(f"Current Dir: {curr_dir}")
-    player_row, player_column = curr_dir
+def move(move_direction, direction):
+    if move_direction + direction >= len(directions):
+        move_direction = 0
+        return move_direction
+    
+    elif direction == 0:
+        logging.debug(f"Move Direction: {move_direction}")
+        return move_direction
+    
+    elif direction >= 1:
+        logging.debug(f"Move Direction: {move_direction}")
+        return move_direction + 1
+
+def main(curr_dir: int, start_pos: tuple = None, grid = grid):
+    logging.debug(f"Current Dir: {directions[curr_dir]}")
+    previous_grid = grid
+    player_row, player_column = directions[curr_dir]
     for row_index, row in enumerate(grid):
-
         for column_index, item in enumerate(row):
+            if previous_grid != grid:
+                logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
+                logging.debug("----------------------------")
+                for row in grid:
+                    logging.debug(row)
+                logging.debug("----------------------------")
             if item == 2:
-                print(f"row: {row_index + player_row}   |   column: {column_index + player_column}")
-
-                if grid[row_index + player_row][column_index + player_column] == 1:
-                    curr_dir = directions["right"]
-
-                if row_index + player_row < len(grid) and row_index + player_row >= 0 and column_index + player_column < len(row) and column_index + player_column >= 0: #Doesnt care if there is a 1 in the way but does go all the way up now
-                    print("HIT")
-                    grid[row_index][column_index] = 0
-                    grid[row_index + player_row][column_index + player_column] = 2
-
-                    
+                if row_index + player_row < len(previous_grid) and row_index + player_row >= 0 and column_index + player_column < len(row) and column_index + player_column >= 0: #Doesnt care if there is a 1 in the way but does go all the way up now
+                    #logging.debug(f"Next position in previous_grid: {previous_grid[row_index + player_row][column_index + player_column]}")
+                    if previous_grid[row_index + player_row][column_index + player_column] == 1:
+                        curr_dir = move(curr_dir, 1)                        
+                    else: 
+                        previous_grid[row_index][column_index] = 0
+                        previous_grid[row_index + player_row][column_index + player_column] = 2
 
                 else:
-                    return False
-                
-    print("----------------------------")
-    for row in grid:
-        print(row)
-    print("----------------------------")
+                    return curr_dir, grid 
 
-    time.sleep(2)
-    return True
+    grid = previous_grid 
+
+    logging.debug(f"row: {row_index + player_row}   |   column: {column_index + player_column}")          
+    logging.debug("----------------------------")
+    for row in grid:
+        logging.debug(row)
+    logging.debug("----------------------------")
+
+    #time.sleep(2)
+    return curr_dir, grid
 
 
     
 
 
-edge_hit = True
+running = True
+i = 0
+while i <= 50:
+    current_direction, grid = main(current_direction)
+    i += 1
 
-while edge_hit:
-    edge_hit = move()
 
-print("----------------------------")
+logging.debug("----------------------------")
 for row in grid:
-    print(row)
-print("----------------------------")
+    logging.debug(row)
+logging.debug("----------------------------")
