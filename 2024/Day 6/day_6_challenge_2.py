9173828213704fc0975f91f7aed5f084d4679d2b
RWilliams27 <rwilliams@jwplayer.com>
2024-12-12 15:39:03 +0000

    Created a list with all possible obstacle positions.
    I just need to figure out the logic for detecting when a loop exists

--- 2024/Day 6/day_6_challenge_2.py
+++ 2024/Day 6/day_6_challenge_2.py
@@ -1,10 +1,10 @@
 from day_6_input_data import input_data
-import logging, time
+import logging, time, copy
 
 ## Challenge URL: https://adventofcode.com/2024/day/6#part2
 
 logging.basicConfig(
-    filename="Day_6_Challenge_1.log",
+    filename="Day_6_Challenge_2.log",
     encoding="utf-8",
     filemode="a",
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -73,43 +73,55 @@ def move(move_direction, direction):
         #logging.debug(f"Move Direction: {move_direction}")
         return move_direction + 1
 
-def main(curr_dir: int, grid = grid):
+def main(curr_dir: int, grid, checked = None):
+
+    max_check = 200 #len(grid) * len(grid[0])
     running = True
     previous_grid = grid
     player_row, player_column = directions[curr_dir]
-    #logging.debug(f"Current Dir: {directions[curr_dir]}")
+    exit_found = False
+
+    while checked <= max_check:
+        for row_index, row in enumerate(grid):
+            for column_index, item in enumerate(row):
+                checked += 1
+                print(f"Checked: {checked}")
+                if item == 2:
+                    if row_index + player_row < len(grid) and row_index + player_row >= 0 and column_index + player_column < len(row) and column_index + player_column >= 0: 
+                        if grid[row_index + player_row][column_index + player_column] == 1 or grid[row_index + player_row][column_index + player_column] == 4:
+                            curr_dir = move(curr_dir, 1)         
+                            return curr_dir, grid, running, exit_found, checked        
+                        else: 
+                            previous_grid[row_index][column_index] = 3
+                            previous_grid[row_index + player_row][column_index + player_column] = 2
+                            return curr_dir, grid, running, exit_found, checked  
+
+                    else:
+                        running = False
+                        exit_found = True
+                        return curr_dir, grid, running, exit_found, checked  
+
+        grid = previous_grid 
+
+        return curr_dir, grid, running, exit_found, checked   
+    else:
+        running = False
+        return curr_dir, grid, running, exit_found, checked   
+
+
+def grid_maker(og_grid): # Makes copies of the grid, each copy having the obstacle in a different place
+    new_grid = []
+    template_grid = copy.deepcopy(og_grid)
+    
+    for row_index, row in enumerate(og_grid):
+        for column_index, column in enumerate(row):  
+            if og_grid[row_index][column_index] == 1 or og_grid[row_index][column_index] == 2:
+                continue        
+            temp_grid = copy.deepcopy(template_grid)
+            temp_grid[row_index][column_index] = 4
+            new_grid.append(temp_grid)
             
-    #logging.debug("-----------------------------")
-    #for row in grid:
-        #logging.debug(row)
-    #logging.debug("-----------------------------")  
-    for row_index, row in enumerate(grid):
-        for column_index, item in enumerate(row):
-            if item == 2:
-                if row_index + player_row < len(grid) and row_index + player_row >= 0 and column_index + player_column < len(row) and column_index + player_column >= 0: 
-                    if grid[row_index + player_row][column_index + player_column] == 1:
-                        #logging.debug(f"Player Row: {player_row}    |   Player Column: {player_column}")
-                        curr_dir = move(curr_dir, 1)           
-                        #logging.debug("HIT 2")   
-                        return curr_dir, grid, running        
-                    else: 
-                        #logging.debug(f"Player Row: {player_row}    |   Player Column: {player_column}")
-                        previous_grid[row_index][column_index] = 3
-                        previous_grid[row_index + player_row][column_index + player_column] = 2
-
-                        #logging.debug("HIT 3")
-                        return curr_dir, grid, running
-
-                else:
-                    #logging.debug("HIT 4")
-                    running = False
-                    return curr_dir, grid, running
-
-    grid = previous_grid 
-
-    #logging.debug(f"Row Index + Player Row: {row_index + player_row}   |   Column Index + Player Column: {column_index + player_column}")  
-    return curr_dir, grid, running
-
+    return new_grid
 
 def final_path_count(grid: list, final_count: int):
     for row_index, row in enumerate(grid):
@@ -117,22 +129,32 @@ def final_path_count(grid: list, final_count: int):
             if grid[row_index][column_index] == 3:
                 final_count += 1
     
-    return final_count
-    
-running = True
+    return final_count    
+
+new_grid = grid_maker(grid)
+
+for grid in new_grid:
+    logging.debug("-------------------------FINAL GRID----------------------")
+    for row in grid:
+        logging.debug(row)
+    logging.debug("-------------------------FINAL GRID----------------------")
+
+
+print(f"New Grid Length: {len(new_grid)}")
+
 i = 0
-while running:
-    current_direction, grid, running = main(current_direction)
+loops = 0
+
+while i < len(new_grid):
+    
+
     i += 1
 
-final_count = 1
-final_count = final_path_count(grid, final_count)
+print(f"Loops is: {loops}")
 
-logging.debug("----------------------------")
-for row in grid:
-    logging.debug(row)
-logging.debug("----------------------------")
 
-end = time.time()
-logging.debug(f"Time Taken: {end - start}")
-logging.debug(f"Final Count: {final_count}")
+for grid in new_grid:
+    logging.debug("-------------------------FINAL OTHER GRID----------------------")
+    for row in grid:
+        logging.debug(row)
+    logging.debug("-------------------------FINAL OTHER GRID----------------------")
